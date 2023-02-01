cf1d1bf7fda9a5c59eca5b5fdbaa6a1492e22be6
pp_chjz <pirapat.j@ku.th>
2023-02-01 22:26:09 +0700

    set up chakra

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="register">
-    <h1>This is an register page</h1>
+  <div>
+    <div class="register">
+      <h1>This is an register page</h1>
+      <p>Email</p>
+      <input v-model="message" placeholder="Email" /> 
+      <p>Username</p>
+      <input v-model="message" placeholder="Username" />
+      <p>Password</p>
+      <input v-model="message" placeholder="Password" />
+      
+    </div> <br>
+    <c-button variant-color="blue" variant="outline" size="sm" @click="summit">
+        summit
+    </c-button>
   </div>
 </template>
+
+<script>
+import MenuApi from "@/store/MenuApi.js"
+import { CButton } from "@chakra-ui/vue"
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    CButton,
+  },
+  data(){
+    return{
+      message:"",
+      menu: "",
+      menus:{},
+    }
+  },
+  async created(){
+    console.log("test")
+    await MenuApi.dispatch("fetchMenu")
+      this.menus = MenuApi.getters.getMenus
+      console.log(this.menus)
+    await this.fetchMenu()
+  },
+  methods:{
+    async fetchMenu(){
+      console.log("fetchMenu.")
+      await MenuApi.dispatch("fetchMenu")
+      this.menus = MenuApi.getters.getMenus
+      console.log(this.menus)
+    },
+    summit(){
+      console.log("summit")
+    }
+  }
+}
+</script>
