e686d28fa56ba6e6d16635fa87ab4162580687d2
pp_chjz <pirapat.j@ku.th>
2023-02-14 19:13:48 +0700

    edit menu

---
+++ src/views/EditMenuView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <edit-menu></edit-menu>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EditMenu from '@/components/edit-menu/EditMenu.vue'
+
+export default {
+  name: 'First',
+  components: {
+    EditMenu,
+  }
+}
+</script>
